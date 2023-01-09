04e8a83c95a166e86607efb8b6e8c7434e0d50df
thanujakaviti <thanujakaviti@gmail.com>
2023-01-09 15:25:56 +0530

    project uploaded

---
+++ src/main/java/in/jt/rest/WelcomeRestController.java
@@ -0,0 +1,13 @@
+package in.jt.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+	
+	@GetMapping("/welcome")
+	public String getWelcomeMsg() {
+		return "welcome to java techniques";
+	}
+}
---
+++ src/test/java/in/jt/SpringBootAppn108BasicAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package in.jt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppn108BasicAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
